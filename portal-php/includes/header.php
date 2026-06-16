39f9859c7c784dab46946cf7a7495bb85ca99870
raposovitao <humanov205@gmail.com>
2026-06-16 00:29:37 -0300

    Ajeitando header

--- portal-php/includes/header.php
+++ portal-php/includes/header.php
@@ -1,9 +1,8 @@
 <?php
-// Descobre o nome do arquivo que está sendo acessado no momento
 $paginaAtual = basename($_SERVER['PHP_SELF']);
 ?>
 
 <header class="d-flex justify-content-start border-bottom-3 bg-white py-2">
     <img class="" src="../assets/imagens/logo-unialfa.png" style="width: 200px;" alt="logo unialfa">
-    <a href="index.php" class="mx-4 link-secondary text-decoration-none fw-bold align-self-center">Portal de Estágios</a>
+    <span class="mx-4 link-secondary fw-bold align-self-center">Portal de Estágios</span>
 </header>
